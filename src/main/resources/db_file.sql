968aeebda293fc50d1684a691d7f57cb1c359cce
vikasbelote <vikasb0205@gmail.com>
2017-10-01 10:59:48 +0530

    Adding test data for general head

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,9 +1,3 @@
 
-insert into `society_app`.`sa_general_head_section` (`section_id`, `section_name`) 
-VALUES ('1', 'LIABILITIES & CAPITALS'); 
-
-insert into `society_app`.`sa_general_head_section` (`section_id`, `section_name`) 
-VALUES ('2', 'ASSETS'); 
-
-
-INSERT INTO `society_app`.`sa_general_head` (`general_head_id`, `general_head_name`, `section_id`, `is_default`) VALUES ('11', 'SUNDRY DEBITORS', '2', 1);
+INSERT INTO `society_app`.`sa_general_head` (`general_head_name`, `section_id`, `is_default`)
+VALUES ('SUNDRY DEBITORS', '2', 1);
