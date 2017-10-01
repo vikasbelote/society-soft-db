9b32ded620c2318a17633feef77582f75f78c18f
vikasbelote <vikasb0205@gmail.com>
2017-10-01 10:56:24 +0530

    Adding balance sheet sectiondata

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,1 +1,9 @@
+
+insert into `society_app`.`sa_general_head_section` (`section_id`, `section_name`) 
+VALUES ('1', 'LIABILITIES & CAPITALS'); 
+
+insert into `society_app`.`sa_general_head_section` (`section_id`, `section_name`) 
+VALUES ('2', 'ASSETS'); 
+
+
 INSERT INTO `society_app`.`sa_general_head` (`general_head_id`, `general_head_name`, `section_id`, `is_default`) VALUES ('11', 'SUNDRY DEBITORS', '2', 1);
