4223d5597c81ab8be113c0721ec64ff9acbd2f37
vikasbelote <vikasb0205@gmail.com>
2017-10-11 23:31:47 +0530

    db change

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,14 +1,4 @@
-create table sa_transaction_description
-(
-	 desc_id int auto_increment
-    ,transaction_description varchar(500)
-    ,general_head_id int
-    ,society_id int
-    
-    ,primary key(desc_id)
-    ,foreign key(general_head_id)references sa_general_head(general_head_id)on delete set null on update cascade
-    ,foreign key(society_id)references sa_society(society_id)on delete set null on update cascade
-);
+
 
 drop table sa_transaction;
 
