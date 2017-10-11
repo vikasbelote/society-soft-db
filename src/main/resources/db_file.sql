2b2eaed0c930860d2bb51c4e1e603029f08abc95
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-10-11 17:39:26 +0530

    Changing structure of transaction

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,21 +1,27 @@
-set foreign_key_checks = 0;
-drop table sa_transaction;
-set foreign_key_checks = 1;
+create table sa_transaction_description
+(
+	 desc_id int auto_increment
+    ,transaction_description varchar(500)
+    ,general_head_id int
+    ,society_id int
+    
+    ,primary key(desc_id)
+    ,foreign key(general_head_id)references sa_general_head(general_head_id)on delete set null on update cascade
+    ,foreign key(society_id)references sa_society(society_id)on delete set null on update cascade
+);
 
-CREATE TABLE `sa_transaction` (
+drop table sa_transaction;
+
+create table sa_transaction
+(
   `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
   `general_head_id` int(11) DEFAULT NULL,
-  `transaction_type_id` int(11) DEFAULT NULL,
+  `transaction_description_id` int(11) DEFAULT NULL,
   `transaction_amount` double DEFAULT NULL,
-  `transaction_description` varchar(500) DEFAULT NULL,
   `transaction_date` datetime DEFAULT NULL,
   `society_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`transaction_id`),
-  KEY `society_id` (`society_id`),
-  KEY `FKrmuwans8y5ncks6lxtk341mrw` (`general_head_id`),
-  KEY `transaction_type_id` (`transaction_type_id`),
-  CONSTRAINT `FKrmuwans8y5ncks6lxtk341mrw` FOREIGN KEY (`general_head_id`) REFERENCES `sa_general_head` (`general_head_id`),
-  CONSTRAINT `sa_transaction_ibfk_1` FOREIGN KEY (`general_head_id`) REFERENCES `sa_general_head` (`general_head_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `sa_transaction_ibfk_2` FOREIGN KEY (`society_id`) REFERENCES `sa_society` (`society_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `sa_transaction_ibfk_3` FOREIGN KEY (`transaction_type_id`) REFERENCES `sa_transaction_type` (`type_id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+   PRIMARY KEY (`transaction_id`),
+   FOREIGN KEY (`general_head_id`) REFERENCES `sa_general_head` (`general_head_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+   FOREIGN KEY (`society_id`) REFERENCES `sa_society` (`society_id`) ON DELETE SET NULL ON UPDATE CASCADE,
+   FOREIGN KEY (`transaction_description_id`) REFERENCES `sa_transaction_description` (`desc_id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
