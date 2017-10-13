cec254136c55ad7e296f905252a5288c9c6d6be8
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-10-13 20:57:16 +0530

    sdf

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,17 +1,19 @@
-
-
-drop table sa_transaction;
-
-create table sa_transaction
+create table sa_report
 (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
-  `general_head_id` int(11) DEFAULT NULL,
-  `transaction_description_id` int(11) DEFAULT NULL,
-  `transaction_amount` double DEFAULT NULL,
-  `transaction_date` datetime DEFAULT NULL,
-  `society_id` int(11) DEFAULT NULL,
-   PRIMARY KEY (`transaction_id`),
-   FOREIGN KEY (`general_head_id`) REFERENCES `sa_general_head` (`general_head_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-   FOREIGN KEY (`society_id`) REFERENCES `sa_society` (`society_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-   FOREIGN KEY (`transaction_description_id`) REFERENCES `sa_transaction_description` (`desc_id`) ON DELETE SET NULL ON UPDATE CASCADE
+	 report_id int auto_increment
+	,report_name varchar(100)
+    ,primary key(report_id)
 );
+
+INSERT INTO `society_app`.`sa_report` (`report_id`, `report_name`) VALUES ('1', 'Balance Sheet');
+INSERT INTO `society_app`.`sa_report` (`report_id`, `report_name`) VALUES ('2', 'Income & Expense');
+
+alter table sa_general_head_section add report_id int;
+alter table sa_general_head_section add foreign key(report_id)references sa_report(report_id)on delete set null on update cascade;
+
+
+UPDATE `society_app`.`sa_general_head_section` SET `report_id`='1' WHERE `section_id`='1';
+UPDATE `society_app`.`sa_general_head_section` SET `report_id`='1' WHERE `section_id`='2';
+
+INSERT INTO `society_app`.`sa_general_head_section` (`section_id`, `section_name`, `report_id`) VALUES ('3', 'EXPENSES', '2');
+INSERT INTO `society_app`.`sa_general_head_section` (`section_id`, `section_name`, `report_id`) VALUES ('4', 'INCOME', '2');
