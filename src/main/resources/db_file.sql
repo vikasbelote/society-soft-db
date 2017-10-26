bc3eefe87cc98f64df323f1c46b826a4576b876d
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-10-26 18:17:21 +0530

    Adding society config parameter

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,28 +1,3 @@
-create table sa_report
-(
-	 report_id int auto_increment
-	,report_name varchar(100)
-    ,primary key(report_id)
-);
+alter table sa_society_config add maintenance_payment_due_interest int;
 
-INSERT INTO `society_app`.`sa_report` (`report_id`, `report_name`) VALUES ('1', 'Balance Sheet');
-INSERT INTO `society_app`.`sa_report` (`report_id`, `report_name`) VALUES ('2', 'Income & Expense');
-
-alter table sa_general_head_section add report_id int;
-alter table sa_general_head_section add foreign key(report_id)references sa_report(report_id)on delete set null on update cascade;
-
-
-UPDATE `society_app`.`sa_general_head_section` SET `report_id`='1' WHERE `section_id`='1';
-UPDATE `society_app`.`sa_general_head_section` SET `report_id`='1' WHERE `section_id`='2';
-
-INSERT INTO `society_app`.`sa_general_head_section` (`section_id`, `section_name`, `report_id`) VALUES ('3', 'EXPENSES', '2');
-INSERT INTO `society_app`.`sa_general_head_section` (`section_id`, `section_name`, `report_id`) VALUES ('4', 'INCOME', '2');
-
--------------------------------------------------------
-
-INSERT INTO `society_app`.`sa_report` (`report_id`, `report_name`) VALUES ('3', 'Maintenance');
-INSERT INTO `society_app`.`sa_general_head_section` (`section_id`, `section_name`, `report_id`) VALUES ('5', 'MAINTENANCE', '3');
-
-alter table sa_society_config add start_date date;
-alter table sa_society_config add end_date date;
-alter table sa_society_config add maintenance_cycle int;
+alter table sa_society_config add maintenance_payment_cheque_name varchar(500);
