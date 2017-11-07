fa6b88f684b0fcdfb1cf5552deffb8ecb482dd8b
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-07 18:30:06 +0530

    Addung menu

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,3 +1,15 @@
-alter table sa_society_config add maintenance_payment_due_interest int;
+create table sa_mail_status
+(
+	 mail_status_id int auto_increment
+	,society_id int
+    ,receipt_id int
+    ,mail_status bit
+    ,email_type varchar(10)
+    ,send_date datetime
+    ,primary key(mail_status_id)
+    ,foreign key(society_id)references sa_society(society_id) on delete set null on update cascade
+	,foreign key(receipt_id)references sa_maintenance_receipt(receipt_id) on delete set null on update cascade
+);
 
-alter table sa_society_config add maintenance_payment_cheque_name varchar(500);
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('12', 'Email');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('13', 'Message');
