11a8de8f2bf01c20f71286339c7b1977cc7a08df
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-09 20:53:22 +0530

    Adding area, menu

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,15 +1,30 @@
-create table sa_mail_status
+create table sa_society_additional_area
 (
-	 mail_status_id int auto_increment
-	,society_id int
-    ,receipt_id int
-    ,mail_status bit
-    ,email_type varchar(10)
-    ,send_date datetime
-    ,primary key(mail_status_id)
-    ,foreign key(society_id)references sa_society(society_id) on delete set null on update cascade
-	,foreign key(receipt_id)references sa_maintenance_receipt(receipt_id) on delete set null on update cascade
+	 area_id int auto_increment
+    ,area_name varchar(100)
+    ,society_id int
+    
+    ,primary key(area_id)
+    ,foreign key(society_id)references sa_society(society_id)on delete set null on update cascade
 );
 
-INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('12', 'Email');
-INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('13', 'Message');
+alter table sa_society_member add wing_number varchar(100);
+alter table sa_society_member add flat_number varchar(50);
+alter table sa_society_member add square_feet int;
+alter table sa_society_member add additional_area_id int;
+alter table sa_society_member add foreign key(additional_area_id)references sa_society_additional_area(area_id)on delete set null on update cascade;
+
+
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('14', 'Head');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('15', 'Interest policy');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('16', 'Penalties policy');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('17', 'Rebate policy');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('18', 'Additional Area');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('19', 'Member');
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('20', 'Additional Area');
+
+alter table sa_society add registration_no varchar(200);
+
+INSERT INTO `society_app`.`sa_society_additional_area` (`area_id`, `area_name`) VALUES ('1', 'Terrace');
+INSERT INTO `society_app`.`sa_society_additional_area` (`area_id`, `area_name`) VALUES ('2', 'Mezzanine Floors');
+INSERT INTO `society_app`.`sa_society_additional_area` (`area_id`, `area_name`) VALUES ('3', 'Other');
