75b2c8e1e6962cb9d4eeac2b4f876f1393464d44
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-17 18:17:36 +0530

    Adding db file

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,17 +1,2 @@
-set foreign_key_checks=0;
-
-drop table sa_maintenance_charge;
-
-set foreign_key_checks=1;
-
-create table sa_maintenance_charge
-(
-	 charge_id int auto_increment
-    ,charge_value double
-    ,maintenance_head_id int
-    ,receipt_id int
-    
-    ,primary key(charge_id)
-    ,foreign key(maintenance_head_id)references sa_maintenance_head(head_id)on delete set null on update cascade
-    ,foreign key(receipt_id)references sa_maintenance_receipt(receipt_id)on delete set null on update cascade
-);
+INSERT INTO `society_app`.`sa_menu` (`menu_id`, `menu_name`) VALUES ('21', 'Bill Status');
+alter table sa_maintenance_receipt add bill_status bit;
