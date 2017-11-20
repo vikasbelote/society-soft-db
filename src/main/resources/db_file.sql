52b2611f0842a8f4fbe995a6e8c4fb652f8e111a
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-20 20:56:44 +0530

    Adding column

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,3 +1,4 @@
-alter table sa_maintenance_head add is_default bit;
+alter table sa_maintenance_head add head_code varchar(10);
+alter table sa_maintenance_receipt add out_amount double;
+alter table sa_maintenance_receipt add is_active bit;
 
-alter table sa_maintenance_receipt add total_amount double;
