e06fbe106fade2fca9663ce3072c68e741666684
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-21 17:45:04 +0530

    Altering column

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,4 +1,2 @@
-alter table sa_maintenance_head add head_code varchar(10);
-alter table sa_maintenance_receipt add out_amount double;
-alter table sa_maintenance_receipt add is_active bit;
-
+alter table sa_maintenance_cycle add is_active bit;
+alter table sa_maintenance_receipt add paid_amount double;
