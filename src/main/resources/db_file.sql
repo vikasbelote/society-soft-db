f40f2661563eace50ab2ed099ccb89b26569860c
Bhaskar Belote <vikas.bhaskar.belote@accenture.com>
2017-11-24 20:59:24 +0530

    Adding file upload table

--- src/main/resources/db_file.sql
+++ src/main/resources/db_file.sql
@@ -1,2 +1,13 @@
-alter table sa_maintenance_cycle add is_active bit;
-alter table sa_maintenance_receipt add paid_amount double;
+create table sa_file
+(
+	 file_id int auto_increment
+    ,file_name varchar(500)
+    ,file_type varchar(10)
+    ,uploaded_by_id int
+    ,uploaded_on date
+    ,society_id int
+    
+    ,primary key(file_id)
+    ,foreign key(society_id)references sa_society(society_id)on delete set null on update cascade
+    ,foreign key(uploaded_by_id)references sa_user(user_id) on delete set null on update cascade
+);
